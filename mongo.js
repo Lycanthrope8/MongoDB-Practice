aaa0247e51e35864ff1e4c18e090a24c5578ea9d
Jubayer Hossain <jubayerhossain9999@gmail.com>
2024-02-22 00:05:58 +0600

    complete

--- mongo.js
+++ mongo.js
@@ -1,7 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 
 const url =
-  'mongodb+srv://jubayer:XEZUyGys7HRUOn4S@cluster0.evmbwuh.mongodb.net/places_test?retryWrites=true&w=majority';
+  'mongodb+srv://manu:KyOP1JrHoErqQILt@cluster0-g8eu9.mongodb.net/products_test?retryWrites=true&w=majority';
 
 const createProduct = async (req, res, next) => {
   const newProduct = {
@@ -22,7 +22,22 @@ const createProduct = async (req, res, next) => {
   res.json(newProduct);
 };
 
-const getProducts = async (req, res, next) => {};
+const getProducts = async (req, res, next) => {
+  const client = new MongoClient(url);
+
+  let products;
+
+  try {
+    await client.connect();
+    const db = client.db();
+    products = await db.collection('products').find().toArray();
+  } catch (error) {
+    return res.json({message: 'Could not retrieve products.'});
+  };
+  client.close();
+
+  res.json(products);
+};
 
 exports.createProduct = createProduct;
 exports.getProducts = getProducts;
